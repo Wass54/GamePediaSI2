f28fdb47769935de7526f512bb4d7d81e8fd681e
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-03-24 10:24:02 +0100

    Update index.php

--- ContenuSeance/index.php
+++ ContenuSeance/index.php
@@ -58,3 +58,53 @@ echo 'Nombre de requêtes executées : ' . $compteur;
 **/
 
 $rm->creationOf2UsersWith3CommentsGame12342();
+
+
+
+$fakerFr = Faker\Factory::create('fr_FR');
+$fakerIt = Faker\Factory::create('it_IT');
+$fakerEs = Faker\Factory::create('es_ES');
+$fakerEn = Faker\Factory::create();
+for($i=0;$i<25000;$i++){
+
+    switch (rand(0,10)){
+
+    $u = new User();
+
+        case 1:
+            echo "français :" . "<br>";
+            echo $fakerFr->firstName() . "<br>";
+            echo $fakerFr->lastName() . "<br>";
+            echo $fakerFr->address() . "<br>";
+            echo $fakerFr->email() . "<br>";
+            echo $fakerEn->date() . "<br>";
+            echo $fakerFr->text() . "<br>";
+            break;
+        case 2:
+            echo "Italien :" . "<br>";
+            echo $fakerIt->firstName() . "<br>";
+            echo $fakerIt->lastName() . "<br>";
+            echo $fakerIt->address() . "<br>";
+            echo $fakerIt->email() . "<br>";
+            echo $fakerIt->text() . "<br>";
+            break;
+        case 3:
+            echo "Espagnol :" . "<br>";
+            echo $fakerEs->firstName() . "<br>";
+            echo $fakerEs->lastName() . "<br>";
+            echo $fakerEs->address() . "<br>";
+            echo $fakerEs->email() . "<br>";
+            echo $fakerEs->text() . "<br>";
+            break;
+        default:
+            echo "Englais :" . "<br>";
+            echo $fakerEn->firstName() . "<br>";
+            echo $fakerEn->lastName() . "<br>";
+            echo $fakerEn->address() . "<br>";
+            echo $fakerEn->email() . "<br>";
+            echo $fakerEn->text() . "<br>";
+            break;
+    }
+
+    echo "______________________ " . $i . "______________________ <br>";
+}
