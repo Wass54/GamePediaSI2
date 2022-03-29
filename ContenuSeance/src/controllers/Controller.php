875724d85e0672f365a41604c22a4ec2d8aed9bd
Paul deveaux <paul.deveaux1@gmail.com>
2022-03-29 14:46:38 +0200

    Debut api

---
+++ ContenuSeance/src/controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace game\controller;
+
+use game\models\Game;
+
+class Controller
+{
+
+    public function gameById($rq, $rs, $args){
+        $id = $rq->getQueryParam("id");
+        $rs = $rs->withHeader('Content-Type', 'application/json');
+
+        $game = Game::where('id',$id);
+        $array = array('id' => $game->id, 'name' => $game->name, 'alias' => $game->alias, 'deck' => $game->deck,
+            'description' => $game->description, 'original_release_date' => $game->original_release_date);
+
+        $json = json_encode($array);
+        $rs = $rs->withJson($json);
+        return $rs;
+    }
+}
