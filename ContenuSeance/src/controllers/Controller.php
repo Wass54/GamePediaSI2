bfbc03ee61f55a21f55fad5930d6390a7874171c
Wassim <wassim88881@outlook.fr>
2022-03-29 16:07:44 +0200

    a

--- ContenuSeance/src/controllers/Controller.php
+++ ContenuSeance/src/controllers/Controller.php
@@ -25,14 +25,17 @@ class Controller
     public function allGames($rq, $rs, $args){
         $rs = $rs->withHeader('Content-Type', 'application/json');
         $game = Game::all();
-
-
-        $array = array('id' => $game->id, 'name' => $game->name, 'alias' => $game->alias, 'deck' => $game->deck,
-            'description' => $game->description);
-
-        $json = json_encode($array);
-        $rs = $rs->withJson($json);
-        return $rs;
+        $compteur = 0;
+        foreach($game as $g){
+            if(compteur <= 200){
+                $array = array('id' => $g->id, 'name' => $g->name, 'alias' => $g->alias, 'deck' => $g->deck,
+                'description' => $g->description);
+                $json = json_encode($array);
+                $rs = $rs->withJson($json);
+                $compteur++;
+                return $rs;
+            }
+        }
     }
 
 }
