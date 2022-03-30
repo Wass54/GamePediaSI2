f05ce867fd486b1ca83caa2cf8926dfb856d8c52
Wassim <wassim88881@outlook.fr>
2022-03-30 14:56:39 +0200

    a

--- ContenuSeance/src/models/Game.php
+++ ContenuSeance/src/models/Game.php
@@ -16,12 +16,6 @@ class Game extends Model{
         return $this->belongsToMany(Character::class, 'Game2character', "game_id", "character_id");
     }
 
-    public function platform(){
-
-        return $this->belongsToMany(Game2platform::class, 'game2platform', 'game_id', 'platform_id');
-
-    }
-
     public function ratings()
     {
         return $this->belongsToMany(Game_rating::class, 'game2rating', "game_id", "rating_id");
